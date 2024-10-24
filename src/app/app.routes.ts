41a61cb8a6d2e82c097d87d2fec868df340af9c9
RomanVintoniak <romanvintoniak03@gmail.com>
2024-10-24 17:43:43 +0300

    Add routes & update home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistrationComponent } from './components/auth/registration/registration.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent }
+];
