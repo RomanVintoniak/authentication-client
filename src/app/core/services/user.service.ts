5f76d9e5842da5c292df8f5ac6fb776148f49a39
RomanVintoniak <romanvintoniak03@gmail.com>
2024-11-06 00:10:29 +0200

    Add environment files

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "../models/user.model";
+import { environment } from "../../../environments/environment.development";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
   private readonly http = inject(HttpClient);
-  private readonly baseUrl = 'https://localhost:7128/api/users';
+  private readonly baseUrl = environment.baseUrl;
 
   get(id: string): Observable<User> {
-    return this.http.get<User>(`${this.baseUrl}/${id}`)
+    return this.http.get<User>(`${this.baseUrl}/users/${id}`)
   }
 }
