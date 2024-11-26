55e2b18e01d2e63404c0724cf37701e08dee10bd
RomanVintoniak <romanvintoniak03@gmail.com>
2024-11-26 16:41:25 +0200

    Add NotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegistrationComponent } from './components/auth/registration/registration.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { authGuard } from './core/guards/auth.guard';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: 'users/:id', component: UserProfileComponent, canActivate: [authGuard] }
+  { path: 'users/:id', component: UserProfileComponent, canActivate: [authGuard] },
+  { path: '**', component: NotFoundComponent }
 ];
